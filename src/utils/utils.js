0212690cb38ff869b64c4e5dba1e0f2f0c5a641b
gsf-smclamb <wmclamb@harris.com>
2017-08-03 11:32:40 -0600

    Add comment about return value.

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,7 @@
 // Detects if the current browser is IE.
 const isIE = () => {
   if (!NODE) {
+    // This require call returns the running version of IE or undefined
     const isIE = require('component-ie');
     return isIE;
   }
